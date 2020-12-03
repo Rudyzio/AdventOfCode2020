fe992b02b94ae9a33db58dbaed1087e0601a2547
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-03 23:54:46 +0000

    Solution for Day 03

--- AdventOfCode.Tests/Day03Solver_Tests.cs
+++ AdventOfCode.Tests/Day03Solver_Tests.cs
@@ -1,37 +1,38 @@
+using System;
 using Day_03_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day03Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day03_Input/test.input", 34241)]
-		//[InlineData("Day03_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day03Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day03_Input/test.input", 7)]
+        [InlineData("Day03_Input/puzzle.input", 151)]
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day03Solver.Part1Solution(lines);
+            // Act
+            var result = Day03Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day03_Input/test.input", 51316)]
-	//[InlineData("Day03_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day03_Input/test.input", 336)]
+        [InlineData("Day03_Input/puzzle.input", 7540141059)]
+        public void TestPart2Solution(string inputFile, UInt64 expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day03Solver.Part2Solution(lines);
+            // Act
+            var result = Day03Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 03 Solver/Day03Solver.cs
+++ Day 03 Solver/Day03Solver.cs
@@ -1,15 +1,50 @@
+using System;
+
 namespace Day_03_Solver
-	{
+{
     public static class Day03Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            return TraverseMap(lines, 3, 1);
         }
 
-        public static int Part2Solution(string[] lines)
+        public static double Part2Solution(string[] lines)
         {
-            return 0;
+            // Total result is too big for int or double
+            UInt64 result = Convert.ToUInt64(TraverseMap(lines, 1, 1));
+            result *= Convert.ToUInt64(TraverseMap(lines, 3, 1));
+            result *= Convert.ToUInt64(TraverseMap(lines, 5, 1));
+            result *= Convert.ToUInt64(TraverseMap(lines, 7, 1));
+            result *= Convert.ToUInt64(TraverseMap(lines, 1, 2));
+            return result;
+        }
+
+        private static int TraverseMap(string[] lines, int stepsRight, int stepsDown)
+        {
+            int trees = 0;
+
+            int rowIndex = 0;
+            int rowLength = lines[0].Length;
+            int currentLocation = 0;
+            while (rowIndex < lines.Length)
+            {
+                if (currentLocation >= rowLength)
+                {
+                    currentLocation -= rowLength;
+                }
+
+                var currentRow = lines[rowIndex];
+
+                if (currentRow[currentLocation] == '#')
+                {
+                    trees++;
+                }
+
+                currentLocation += stepsRight;
+                rowIndex += stepsDown;
+            }
+            return trees;
         }
     }
 }
