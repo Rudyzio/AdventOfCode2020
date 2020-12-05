41e6ee1cd80df613542c8cd78bef6a5095ed0ab6
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-05 23:35:24 +0000

    Solution for Day 05

--- AdventOfCode.Tests/Day05Solver_Tests.cs
+++ AdventOfCode.Tests/Day05Solver_Tests.cs
@@ -2,36 +2,38 @@ using Day_05_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day05Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day05_Input/test.input", 34241)]
-		//[InlineData("Day05_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day05Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day05_Input/test1.input", 357)]
+        [InlineData("Day05_Input/test2.input", 567)]
+        [InlineData("Day05_Input/test3.input", 119)]
+        [InlineData("Day05_Input/test4.input", 820)]
+        [InlineData("Day05_Input/puzzle.input", 976)]
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day05Solver.Part1Solution(lines);
+            // Act
+            var result = Day05Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day05_Input/test.input", 51316)]
-	//[InlineData("Day05_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day05_Input/puzzle.input", 685)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day05Solver.Part2Solution(lines);
+            // Act
+            var result = Day05Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 05 Solver/Day05Solver.cs
+++ Day 05 Solver/Day05Solver.cs
@@ -1,15 +1,79 @@
+using System;
+
 namespace Day_05_Solver
-	{
+{
     public static class Day05Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            int placeId = 0;
+            foreach (var line in lines)
+            {
+                (var row, var column) = CalculatePosition(line);
+                var currentSeatID = row * 8 + column;
+                if (currentSeatID > placeId)
+                {
+                    placeId = (int)currentSeatID;
+                }
+            }
+            return placeId;
         }
 
         public static int Part2Solution(string[] lines)
         {
+
+            var rows = 128;
+            var columns = 8;
+            bool[,] seats = new bool[rows, columns];
+
+            foreach (var line in lines)
+            {
+                (var row, var column) = CalculatePosition(line);
+                seats[(int)row, (int)column] = true;
+            }
+
+            for (var i = 1; i < rows - 1; i++)
+            {
+                for (var j = 0; j < columns; j++)
+                {
+                    if (!seats[i, j])
+                    {
+                        return i * 8 + j;
+                    }
+                }
+            }
+
             return 0;
+        }
+
+        private static (double, double) CalculatePosition(string line)
+        {
+            double upperLimitRow = 127;
+            double lowerLimitRow = 0;
+            double upperLimitColumn = 7;
+            double lowerLimitColumn = 0;
+
+            foreach (var position in line)
+            {
+                switch (position)
+                {
+                    case 'F':
+                        upperLimitRow -= Math.Ceiling((upperLimitRow - lowerLimitRow) / 2);
+                        break;
+                    case 'B':
+                        lowerLimitRow += Math.Ceiling((upperLimitRow - lowerLimitRow) / 2);
+                        break;
+                    case 'R':
+                        lowerLimitColumn += Math.Ceiling((upperLimitColumn - lowerLimitColumn) / 2);
+                        break;
+                    case 'L':
+                        upperLimitColumn -= Math.Ceiling((upperLimitColumn - lowerLimitColumn) / 2);
+                        break;
+                }
+                // System.Console.WriteLine($"{position}-{lowerLimitRow}:{upperLimitRow}");
+            }
+
+            return (upperLimitRow, upperLimitColumn);
         }
     }
 }
