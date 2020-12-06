04d98386c8eea8fb4bad66b3c1094988d755359f
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-06 12:06:46 +0000

    Solution for Day 06

--- AdventOfCode.Tests/Day06Solver_Tests.cs
+++ AdventOfCode.Tests/Day06Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_06_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day06Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day06_Input/test.input", 34241)]
-		//[InlineData("Day06_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day06Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day06_Input/test.input", 11)]
+        [InlineData("Day06_Input/puzzle.input", 6583)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day06Solver.Part1Solution(lines);
+            // Act
+            var result = Day06Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day06_Input/test.input", 51316)]
-	//[InlineData("Day06_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day06_Input/test.input", 6)]
+        [InlineData("Day06_Input/puzzle.input", 3290)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day06Solver.Part2Solution(lines);
+            // Act
+            var result = Day06Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 06 Solver/Day06Solver.cs
+++ Day 06 Solver/Day06Solver.cs
@@ -1,15 +1,83 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Day_06_Solver
-	{
+{
     public static class Day06Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            int yesAnswersCount = 0;
+            List<char> yesAnswers = new List<char>();
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    yesAnswersCount += yesAnswers.Count;
+                    yesAnswers.Clear();
+                    continue;
+                }
+
+                foreach (var character in line)
+                {
+                    if (!yesAnswers.Contains(character))
+                    {
+                        yesAnswers.Add(character);
+                    }
+                }
+            }
+            // Last line not counted if lines end without an empty line
+            yesAnswersCount += yesAnswers.Count;
+
+            return yesAnswersCount;
         }
 
         public static int Part2Solution(string[] lines)
         {
-            return 0;
+            int yesAnswersCount = 0;
+            List<char> yesAnswers = new List<char>();
+            List<char> yesTotalAnswers = new List<char>();
+            int peopleInGroup = 0;
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    foreach (var answer in yesAnswers)
+                    {
+                        if (yesTotalAnswers.Count(x => x == answer) == peopleInGroup)
+                        {
+                            yesAnswersCount++;
+                        }
+                    }
+                    yesAnswers.Clear();
+                    yesTotalAnswers.Clear();
+                    peopleInGroup = 0;
+                    continue;
+                }
+
+                foreach (var character in line)
+                {
+                    if (!yesAnswers.Contains(character))
+                    {
+                        yesAnswers.Add(character);
+                    }
+                    yesTotalAnswers.Add(character);
+                }
+
+                peopleInGroup++;
+            }
+            // Last line not counted if lines end without an empty line
+            foreach (var answer in yesAnswers)
+            {
+                if (yesTotalAnswers.Count(x => x == answer) == peopleInGroup)
+                {
+                    yesAnswersCount++;
+                }
+            }
+
+            return yesAnswersCount;
         }
     }
 }
