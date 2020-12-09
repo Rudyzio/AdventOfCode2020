06f3accc8515b6fea0085e3f770f9e57bafa0554
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-09 23:45:35 +0000

    Solution for Day 09

--- AdventOfCode.Tests/Day09Solver_Tests.cs
+++ AdventOfCode.Tests/Day09Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_09_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day09Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day09_Input/test.input", 34241)]
-		//[InlineData("Day09_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day09Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day09_Input/test.input", 127, 5)]
+        [InlineData("Day09_Input/puzzle.input", 556543474, 25)]	
+        public void TestPart1Solution(string inputFile, int expected, int preamble)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day09Solver.Part1Solution(lines);
+            // Act
+            var result = Day09Solver.Part1Solution(lines, preamble);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day09_Input/test.input", 51316)]
-	//[InlineData("Day09_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day09_Input/test.input", 62, 5)]
+        [InlineData("Day09_Input/puzzle.input", 76096372, 25)]
+        public void TestPart2Solution(string inputFile, int expected, int preamble)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day09Solver.Part2Solution(lines);
+            // Act
+            var result = Day09Solver.Part2Solution(lines, preamble);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 09 Solver/Day09Solver.cs
+++ Day 09 Solver/Day09Solver.cs
@@ -1,15 +1,105 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Day_09_Solver
-	{
+{
     public static class Day09Solver
     {
-        public static int Part1Solution(string[] lines)
+        public static long Part1Solution(string[] lines, int preamble)
         {
+            long[] numbers = new long[lines.Length];
+
+            // Read input
+            for (var i = 0; i < lines.Length; i++)
+            {
+                numbers[i] = long.Parse(lines[i]);
+            }
+
+            // Pointer in right place
+            int pointer = preamble;
+
+            while (pointer < numbers.Length)
+            {
+                if (!IsSumOfPreviousPreamble(numbers, pointer, preamble))
+                {
+                    return numbers[pointer];
+                }
+                pointer++;
+            }
+
             return 0;
         }
 
-        public static int Part2Solution(string[] lines)
+        public static long Part2Solution(string[] lines, int preamble)
         {
+            long[] numbers = new long[lines.Length];
+
+            // Read input
+            for (var i = 0; i < lines.Length; i++)
+            {
+                numbers[i] = long.Parse(lines[i]);
+            }
+
+            var step1Solution = Part1Solution(lines, preamble);
+
+            // Get rid of bigger ones and order by biggest first
+            numbers = numbers.Where(x => x < step1Solution && x != step1Solution).ToArray();
+
+            var solutionValues = new List<long>();
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                long intermediateValue = numbers[i];
+                solutionValues = new List<long>() { numbers[i] };
+                for (var j = i + 1; j < numbers.Length; j++)
+                {
+                    if (intermediateValue > intermediateValue + numbers[j])
+                    {
+                        break;
+                    }
+
+                    if (intermediateValue == step1Solution)
+                    {
+                        return solutionValues.Min() + solutionValues.Max();
+                    }
+
+                    intermediateValue += numbers[j];
+                    solutionValues.Add(numbers[j]);
+                }
+            }
+
             return 0;
+        }
+
+        private static bool IsSumOfPreviousPreamble(long[] numbers, int pointer, int preamble)
+        {
+            int lowerSection = pointer - preamble;
+            int higherSection = pointer;
+            // System.Console.WriteLine($"LowerSection {lowerSection}; Higher section {higherSection}; Target number {numbers[pointer]}");
+
+            for (var i = lowerSection; i < higherSection; i++)
+            {
+                if (numbers[i] > numbers[pointer])
+                {
+                    continue;
+                }
+                for (var j = i + 1; j < higherSection; j++)
+                {
+                    if (numbers[j] > numbers[pointer])
+                    {
+                        continue;
+                    }
+                    // System.Console.WriteLine($"{i};{j}");
+                    // System.Console.WriteLine($"{numbers[i]} + {numbers[j]} = {numbers[i] + numbers[j]}");
+                    if (numbers[i] + numbers[j] == numbers[pointer])
+                    {
+                        // System.Console.WriteLine();
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
