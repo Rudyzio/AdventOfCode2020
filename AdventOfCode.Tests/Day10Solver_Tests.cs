eff0867907f2ec3a1af8fc9642b3d07cf7be7a02
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-12 01:39:28 +0000

    Solution for Day 10

--- AdventOfCode.Tests/Day10Solver_Tests.cs
+++ AdventOfCode.Tests/Day10Solver_Tests.cs
@@ -2,36 +2,38 @@ using Day_10_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day10Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day10_Input/test.input", 34241)]
-		//[InlineData("Day10_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day10Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day10_Input/test1.input", 35)]
+        [InlineData("Day10_Input/test2.input", 220)]
+        [InlineData("Day10_Input/puzzle.input", 1820)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day10Solver.Part1Solution(lines);
+            // Act
+            var result = Day10Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day10_Input/test.input", 51316)]
-	//[InlineData("Day10_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day10_Input/test1.input", 8)]
+        [InlineData("Day10_Input/test2.input", 19208)]
+        [InlineData("Day10_Input/puzzle.input", 3454189699072)]
+        public void TestPart2Solution(string inputFile, long expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day10Solver.Part2Solution(lines);
+            // Act
+            var result = Day10Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 10 Solver/Day10Solver.cs
+++ Day 10 Solver/Day10Solver.cs
@@ -1,15 +1,88 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Day_10_Solver
 {
     public static class Day10Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            List<int> adapters = new List<int>();
+
+            foreach (var line in lines)
+            {
+                adapters.Add(int.Parse(line));
+            }
+
+            int oneJoltDiffs = 0;
+            int threeJoltDiffs = 0;
+            int currentAdapter = 0;
+            var currentJoltDifference = 0;
+
+            // Order adapters
+            adapters = adapters.OrderBy(x => x).ToList();
+            // Add my adapter
+            adapters.Add(adapters.Max() + 3);
+
+            foreach (var adapter in adapters)
+            {
+                currentJoltDifference = adapter - currentAdapter;
+                if (currentJoltDifference == 1)
+                {
+                    oneJoltDiffs++;
+                }
+                if (currentJoltDifference == 3)
+                {
+                    threeJoltDiffs++;
+                }
+                currentAdapter = adapter;
+            }
+
+            return oneJoltDiffs * threeJoltDiffs;
         }
 
-        public static int Part2Solution(string[] lines)
+        public static long Part2Solution(string[] lines)
         {
-            return 0;
+            List<int> adapters = new List<int>();
+
+            foreach (var line in lines)
+            {
+                adapters.Add(int.Parse(line));
+            }
+
+            // Order adapters
+            adapters = adapters.OrderBy(x => x).ToList();
+            adapters.Insert(0, 0);
+            // Add my adapter
+            adapters.Add(adapters.Max() + 3);
+
+            var forks = new long[adapters.Count];
+            for (var i = 0; i < forks.Length; i++)
+            {
+                // Always begins with only 1 way
+                if (i == 0)
+                {
+                    forks[i] = 1;
+                }
+                else
+                {
+                    forks[i] = 0;
+                    // Sum the other possibilities that come from behind
+                    for (var j = i - 1; j >= 0; j--)
+                    {
+                        if (adapters[i] - adapters[j] <= 3)
+                        {
+                            forks[i] += forks[j];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return forks[forks.Length - 1];
         }
     }
 }
