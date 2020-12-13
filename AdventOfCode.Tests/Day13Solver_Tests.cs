8c7788aaed084bbca82465fe38fbc8c26fd71fd4
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-13 19:54:31 +0000

    Solution for Day 13

--- AdventOfCode.Tests/Day13Solver_Tests.cs
+++ AdventOfCode.Tests/Day13Solver_Tests.cs
@@ -2,36 +2,41 @@ using Day_13_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day13Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day13_Input/test.input", 34241)]
-		//[InlineData("Day13_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day13Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day13_Input/test.input", 295)]
+        [InlineData("Day13_Input/puzzle.input", 2215)]
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day13Solver.Part1Solution(lines);
+            // Act
+            var result = Day13Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day13_Input/test.input", 51316)]
-	//[InlineData("Day13_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day13_Input/test1.input", 1068781)]
+        [InlineData("Day13_Input/test2.input", 3417)]
+        [InlineData("Day13_Input/test3.input", 754018)]
+        [InlineData("Day13_Input/test4.input", 779210)]
+        [InlineData("Day13_Input/test5.input", 1261476)]
+        [InlineData("Day13_Input/test6.input", 1202161486)]
+        [InlineData("Day13_Input/puzzle2.input", 1058443396696792)]
+        public void TestPart2Solution(string inputFile, long expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day13Solver.Part2Solution(lines);
+            // Act
+            var result = Day13Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 13 Solver/Day13Solver.cs
+++ Day 13 Solver/Day13Solver.cs
@@ -1,15 +1,71 @@
+using System;
+using System.Collections.Generic;
+
 namespace Day_13_Solver
 {
     public static class Day13Solver
     {
+        // Part 1 done at first try!!!
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            int myDepart = int.Parse(lines[0]);
+            int bestBusId = 0;
+            int bestBusWaitingTime = 0;
+
+            var buses = lines[1];
+            foreach (var bus in buses.Split(","))
+            {
+                if (!string.Equals(bus, "x"))
+                {
+                    var currentBusID = int.Parse(bus);
+                    int tripsToMyDepart = (int)Math.Ceiling((double)myDepart / (double)currentBusID);
+                    int currentWaitingTime = currentBusID * tripsToMyDepart;
+
+                    if (bestBusId == 0 && bestBusWaitingTime == 0)
+                    {
+                        bestBusId = currentBusID;
+                        bestBusWaitingTime = currentWaitingTime - myDepart;
+                    }
+                    else
+                    {
+                        if (currentWaitingTime - myDepart < bestBusWaitingTime)
+                        {
+                            bestBusWaitingTime = currentWaitingTime - myDepart;
+                            bestBusId = currentBusID;
+                        }
+                    }
+                }
+            }
+
+            return bestBusId * bestBusWaitingTime;
         }
 
-        public static int Part2Solution(string[] lines)
+        // Part 2 took a long time to figure it out...
+        public static long Part2Solution(string[] lines)
         {
-            return 0;
+            string[] busesSplitted = lines[0].Split(",");
+            long currentTime = 0;
+            long inc = long.Parse(busesSplitted[0]);
+
+            for (var i = 1; i < busesSplitted.Length; i++)
+            {
+                if (!busesSplitted[i].Equals("x"))
+                {
+                    var currentBusID = long.Parse(busesSplitted[i]);
+                    while (true)
+                    {
+                        currentTime += inc;
+                        if ((currentTime + i) % currentBusID == 0)
+                        {
+                            inc *= currentBusID;
+                            break;
+                        }
+                    }
+                    // System.Console.WriteLine($"Time: {currentTime}; Increment: {inc}");
+                }
+            }
+
+            return currentTime;
         }
     }
 }
