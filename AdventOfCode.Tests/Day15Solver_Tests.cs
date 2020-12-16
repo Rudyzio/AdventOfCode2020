0ce8f7884547a45f3d7ade4cea0a998574976c45
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-16 19:01:24 +0000

    Solution for Day 15

--- AdventOfCode.Tests/Day15Solver_Tests.cs
+++ AdventOfCode.Tests/Day15Solver_Tests.cs
@@ -2,36 +2,48 @@ using Day_15_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day15Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day15_Input/test.input", 34241)]
-		//[InlineData("Day15_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day15Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day15_Input/test1.input", 436)]
+        [InlineData("Day15_Input/test2.input", 1)]
+        [InlineData("Day15_Input/test3.input", 10)]
+        [InlineData("Day15_Input/test4.input", 27)]
+        [InlineData("Day15_Input/test5.input", 78)]
+        [InlineData("Day15_Input/test6.input", 438)]
+        [InlineData("Day15_Input/test7.input", 1836)]
+        [InlineData("Day15_Input/puzzle.input", 959)]
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day15Solver.Part1Solution(lines);
+            // Act
+            var result = Day15Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day15_Input/test.input", 51316)]
-	//[InlineData("Day15_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day15_Input/test1.input", 175594)]
+        [InlineData("Day15_Input/test2.input", 2578)]
+        [InlineData("Day15_Input/test3.input", 3544142)]
+        [InlineData("Day15_Input/test4.input", 261214)]
+        [InlineData("Day15_Input/test5.input", 6895259)]
+        [InlineData("Day15_Input/test6.input", 18)]
+        [InlineData("Day15_Input/test7.input", 362)]
+        [InlineData("Day15_Input/puzzle.input", 116590)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day15Solver.Part2Solution(lines);
+            // Act
+            var result = Day15Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 15 Solver/Day15Solver.cs
+++ Day 15 Solver/Day15Solver.cs
@@ -1,15 +1,70 @@
+using System.Collections.Generic;
+
 namespace Day_15_Solver
 {
     public static class Day15Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            return DetermineNumber(lines, 2020);
         }
 
         public static int Part2Solution(string[] lines)
         {
-            return 0;
+            return DetermineNumber(lines, 30000000);
         }
+
+        private static int DetermineNumber(string[] lines, int target)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            var line = lines[0];
+            var splitted = line.Split(",");
+
+            int turn = 1;
+            int lastNumber = 0;
+            int currentNumber = 0;
+            bool lastNumberFirstTime = true;
+
+            foreach (var startingNum in splitted)
+            {
+                lastNumber = int.Parse(startingNum);
+                numbers.Add(lastNumber, turn);
+                turn++;
+            }
+
+            while (turn <= target)
+            {
+                if (lastNumberFirstTime)
+                {
+                    currentNumber = 0;
+                    if (numbers.ContainsKey(currentNumber))
+                    {
+                        lastNumberFirstTime = false;
+                    }
+                    else
+                    {
+                        numbers.Add(currentNumber, turn);
+                    }
+                }
+                else
+                {
+                    currentNumber = turn - 1 - numbers[lastNumber];
+                    numbers[lastNumber] = turn - 1;
+                    if (!numbers.ContainsKey(currentNumber))
+                    {
+                        lastNumberFirstTime = true;
+                        numbers.Add(currentNumber, turn);
+                    }
+
+                }
+
+                turn++;
+                lastNumber = currentNumber;
+            }
+
+            return lastNumber;
+        }
+
     }
 }
