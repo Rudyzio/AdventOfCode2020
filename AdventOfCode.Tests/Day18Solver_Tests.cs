15f75ad1cbfd4a2c822d29e714a748b210d6a7e1
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-20 23:44:59 +0000

    Solution for Day 18

--- AdventOfCode.Tests/Day18Solver_Tests.cs
+++ AdventOfCode.Tests/Day18Solver_Tests.cs
@@ -2,36 +2,46 @@ using Day_18_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day18Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day18_Input/test.input", 34241)]
-		//[InlineData("Day18_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day18Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day18_Input/test1.input", 71)]
+        [InlineData("Day18_Input/test2.input", 51)]
+        [InlineData("Day18_Input/test3.input", 26)]
+        [InlineData("Day18_Input/test4.input", 437)]
+        [InlineData("Day18_Input/test5.input", 12240)]
+        [InlineData("Day18_Input/test6.input", 13632)]
+        [InlineData("Day18_Input/puzzle.input", 4940631886147)]	
+        public void TestPart1Solution(string inputFile, long expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day18Solver.Part1Solution(lines);
+            // Act
+            var result = Day18Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day18_Input/test.input", 51316)]
-	//[InlineData("Day18_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day18_Input/test1.input", 231)]
+        [InlineData("Day18_Input/test2.input", 51)]
+        [InlineData("Day18_Input/test3.input", 46)]
+        [InlineData("Day18_Input/test4.input", 1445)]
+        [InlineData("Day18_Input/test5.input", 669060)]
+        [InlineData("Day18_Input/test6.input", 23340)]
+        [InlineData("Day18_Input/puzzle.input", 283582817678281)]	
+        public void TestPart2Solution(string inputFile, long expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day18Solver.Part2Solution(lines);
+            // Act
+            var result = Day18Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 18 Solver/Day18Solver.cs
+++ Day 18 Solver/Day18Solver.cs
@@ -1,15 +1,119 @@
+using System.Collections.Generic;
+
 namespace Day_18_Solver
 {
     public static class Day18Solver
     {
-        public static int Part1Solution(string[] lines)
+        public static long Part1Solution(string[] lines)
         {
-            return 0;
+            // Shunting Yard Algorithm inspiration
+            long toReturn = 0;
+            Stack<long> numbers = new Stack<long>();
+            Stack<char> operations = new Stack<char>();
+
+            foreach (var line in lines)
+            {
+                operations.Push('(');
+                foreach (var character in line)
+                {
+                    switch (character)
+                    {
+                        case ' ':
+                            break;
+                        case '(':
+                            operations.Push(character);
+                            break;
+                        case ')':
+                            Calculate(numbers, operations, "(");
+                            operations.Pop();
+                            break;
+                        case '+':
+                            Calculate(numbers, operations, "(");
+                            operations.Push(character);
+                            break;
+                        case '*':
+                            Calculate(numbers, operations, "(");
+                            operations.Push(character);
+                            break;
+                        // Is a number
+                        default:
+                            var nextNumber = int.Parse(character.ToString());
+                            numbers.Push(nextNumber);
+                            break;
+                    }
+                }
+                Calculate(numbers, operations, "(");
+                toReturn += numbers.Pop();
+            }
+
+            return toReturn;
         }
 
-        public static int Part2Solution(string[] lines)
+
+        public static long Part2Solution(string[] lines)
         {
-            return 0;
+            long toReturn = 0;
+            Stack<long> numbers = new Stack<long>();
+            Stack<char> operations = new Stack<char>();
+
+            foreach (var line in lines)
+            {
+                operations.Push('(');
+                foreach (var character in line)
+                {
+                    switch (character)
+                    {
+                        case ' ':
+                            break;
+                        case '(':
+                            operations.Push(character);
+                            break;
+                        case ')':
+                            Calculate(numbers, operations, "(");
+                            operations.Pop();
+                            break;
+                        case '+':
+                            Calculate(numbers, operations, "(*");
+                            operations.Push(character);
+                            break;
+                        case '*':
+                            Calculate(numbers, operations, "(");
+                            operations.Push(character);
+                            break;
+                        // Is a number
+                        default:
+                            var nextNumber = int.Parse(character.ToString());
+                            numbers.Push(nextNumber);
+                            break;
+                    }
+                }
+                Calculate(numbers, operations, "(");
+                toReturn += numbers.Pop();
+            }
+            return toReturn;
         }
+
+        private static void Calculate(Stack<long> numbers, Stack<char> operations, string until)
+        {
+            while (!until.Contains(operations.Peek()))
+            {
+                var operation = operations.Pop();
+                var firstOperand = numbers.Pop();
+                var secondOperand = numbers.Pop();
+                switch (operation)
+                {
+                    case '+':
+                        // System.Console.WriteLine($"{firstOperand} + {secondOperand} = {firstOperand + secondOperand}");
+                        numbers.Push(firstOperand + secondOperand);
+                        break;
+                    case '*':
+                        // System.Console.WriteLine($"{firstOperand} * {secondOperand} = {firstOperand * secondOperand}");
+                        numbers.Push(firstOperand * secondOperand);
+                        break;
+                }
+            }
+        }
+
+
     }
 }
