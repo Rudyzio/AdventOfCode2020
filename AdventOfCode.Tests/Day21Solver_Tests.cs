76961fe9040fb4c9dbb1cd6177580e168ac1d31c
Rodrigo Santos <rudyzio92@gmail.com>
2020-12-26 18:25:42 +0000

    Solution for Day 21

--- AdventOfCode.Tests/Day21Solver_Tests.cs
+++ AdventOfCode.Tests/Day21Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_21_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day21Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day21_Input/test.input", 34241)]
-		//[InlineData("Day21_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day21Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day21_Input/test.input", 5)]
+        [InlineData("Day21_Input/puzzle.input", 1885)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day21Solver.Part1Solution(lines);
+            // Act
+            var result = Day21Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day21_Input/test.input", 51316)]
-	//[InlineData("Day21_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day21_Input/test.input", "mxmxvkd,sqjhc,fvjkl")]
+        [InlineData("Day21_Input/puzzle.input", "fllssz,kgbzf,zcdcdf,pzmg,kpsdtv,fvvrc,dqbjj,qpxhfp")]
+        public void TestPart2Solution(string inputFile, string expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day21Solver.Part2Solution(lines);
+            // Act
+            var result = Day21Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 21 Solver/Day21Solver.cs
+++ Day 21 Solver/Day21Solver.cs
@@ -1,15 +1,86 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace Day_21_Solver
 {
     public static class Day21Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            (var suspects, var occurrences) = ParseInput(lines);
+            var allSuspects = suspects.SelectMany(x => x.Value).ToList();
+            var totalOccurrences = occurrences.Where(x => !allSuspects.Contains(x.Key)).Sum(x => x.Value);
+
+            return totalOccurrences;
         }
 
-        public static int Part2Solution(string[] lines)
+        public static string Part2Solution(string[] lines)
         {
-            return 0;
+            (var suspects, var occurrences) = ParseInput(lines);
+
+            var canonicalOrder = suspects.OrderBy(x => x.Key).Select(x => x.Value.First()).ToList().Aggregate((i, j) => i + "," + j);
+
+            return canonicalOrder;
+        }
+
+        private static (Dictionary<string, List<string>>, Dictionary<string, int>) ParseInput(string[] lines)
+        {
+            var suspects = new Dictionary<string, List<string>>();
+            var occurrences = new Dictionary<string, int>();
+
+            foreach (var line in lines)
+            {
+                var suspectString = Regex.Match(line, @"^[^\(]+").Value;
+                var allergenesString = Regex.Match(line, @"\(contains(.*?\))").Groups[1].ToString();
+                var suspectsList = suspectString.Split(" ").Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+                foreach (var suspect in suspectsList)
+                {
+                    if (!occurrences.ContainsKey(suspect))
+                    {
+                        occurrences.Add(suspect, 1);
+                    }
+                    else
+                    {
+                        occurrences[suspect]++;
+                    }
+                }
+
+                foreach (var allergene in allergenesString.Split(","))
+                {
+                    var cleanAllergene = allergene.Replace(")", "").Trim();
+                    if (!suspects.ContainsKey(cleanAllergene))
+                    {
+                        suspects.Add(cleanAllergene, new List<string>());
+                    }
+
+                    if (suspects[cleanAllergene].Count == 0)
+                    {
+                        suspects[cleanAllergene] = suspectsList;
+                    }
+                    else
+                    {
+                        suspects[cleanAllergene] = suspectsList.Intersect(suspects[cleanAllergene]).ToList();
+                    }
+
+                    // Clean from other allergenes
+                    if (suspects[cleanAllergene].Count == 1)
+                    {
+                        var criminal = suspects[cleanAllergene].First();
+
+                        foreach (var storedSuspects in suspects)
+                        {
+                            if (!storedSuspects.Key.Equals(cleanAllergene))
+                            {
+                                suspects[storedSuspects.Key] = storedSuspects.Value.Where(x => !x.Equals(criminal)).ToList();
+                            }
+                        }
+                    }
+                }
+            }
+        
+            return (suspects, occurrences);
         }
     }
 }
